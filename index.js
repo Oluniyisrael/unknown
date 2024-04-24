c4ee94b4e106e7424c56815518fa15939d7cca7b
Oluniyisrael <israeloluniyi2004@gmail.com>
2024-04-24 09:32:26 +0100

    nye

--- index.js
+++ index.js
@@ -1,60 +1,4 @@
-// let slideIndex = 1;
-// showSlides(slideIndex);
 
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-
-// setInterval(() => {
-//     plusSlides(1)
-// }, 5000);
-var slideshows = document.querySelectorAll('[data-component="slideshow"]');
-  
-// Apply to all slideshows that you define with the markup wrote
-slideshows.forEach(initSlideShow);
-
-function initSlideShow(slideshow) {
-
-  var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
-
-  var index = 0, time = 5000;
-  slides[index].classList.add('active');  
-  
-  setInterval( () => {
-    slides[index].classList.remove('active');
-    
-    //Go over each slide incrementing the index
-    index++;
-    
-    // If you go over all slides, restart the index to show the first slide and start again
-    if (index === slides.length) index = 0; 
-    
-    slides[index].classList.add('active');
-
-  }, time);
-}
 window.addEventListener('scroll', ()=> {
   var navbar = document.getElementById('navbar');
   var navbarDis = document.getElementById('navbar2');
@@ -67,6 +11,8 @@ window.addEventListener('scroll', ()=> {
     navbarDis.style.display ='none';
   }
 });
+
+
 // window.addEventListener("keydown",(e)=>{
 //   console.log(e.key)
 // })
